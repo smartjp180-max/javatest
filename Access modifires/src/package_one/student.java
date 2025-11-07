028322dd5db29d002a3b011bc486628f1d4efc77
smartjp180-max <smartjp180@gmail.com>
2025-11-07 11:01:38 +0530

    My Student Program

---
+++ Access modifires/src/package_one/student.java
@@ -0,0 +1,15 @@
+package package_one;
+
+public class student {
+	public static void main(String args[])
+	{
+		teacher t1 = new teacher();
+		System.out.println(t1.t_name);
+	}
+
+}
+
+class teacher
+{
+	public String t_name ="jp";
+}
